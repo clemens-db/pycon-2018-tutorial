8cb65158c583af71f454ac328356348282015851
clemens.mewald@databricks.com <clemens.mewald@databricks.com>
2020-12-18 19:51:26 +0000

    asdf

---
+++ test.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+import pandas as pd
+
+# note that we will set the working directory automatically, this is just for demo purposess
+df = pd.read_csv('/Workspace/Projects/clemens@databricks.com/pycon-2018-tutorial/ted.csv')
+
+# COMMAND ----------
+
+df
+
+# COMMAND ----------
+
